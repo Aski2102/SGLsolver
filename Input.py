17223c3a7824ac031dc8143db3277fdcc7f5c34c
Charlotte M. Marsh <cmarsh@uni-bremen.de>
2018-08-02 17:06:29 +0200

    Created Input.py

---
+++ Input.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Read input file
+
+    This module reads the input file and saves all relevant variables
+
+@author: charlotte
+"""
+
+import numpy as np
+import scipy as sp
+
+
+
+
+
