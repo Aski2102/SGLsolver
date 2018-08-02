3689dcbf940e7cdd91b8060740d85dc54618844f
Charlotte M. Marsh <cmarsh@uni-bremen.de>
2018-08-02 19:00:06 +0200

    Created inputfile folder and wrote inputreader.py
    Inputfile folder for example files. For inputreader.py made a skript that reads inputfile and saves the relevant variables

--- inputreader.py
+++ inputreader.py
@@ -10,6 +10,27 @@ Read input file
 import numpy as np
 import scipy as sp
 
+inputfile = open("inputfiles/schrodinger1.inp", "r")
+
+
+
+data = inputfile.readlines() #reading input file
+
+#declaring variables
+
+mass = float(data[0].split("#")[0].strip()) #mass
+
+minmax = np.array(data[1].split(" ")[0:3], dtype=float) #xMin xMax nPoints
+
+eigenval = np.array(data[2].split(" ")[0:2], dtype=float) #first and last eigenvalue
+
+iptype = data[3].split("#")[0].strip() #interpolation type
+
+nip = int(data[4].split("#")[0].strip()) #number of interpolation points
+
+#ipoints = np.zeros((int(nip),2), dtype=float)
+for ii in range(0, nip):            #interpolation points in an array
+    ipoints[ii,:] = np.array(data[5+ii].split(" "), dtype=float)
 
 
 
