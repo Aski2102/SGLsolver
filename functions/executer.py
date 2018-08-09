fb3ac1ae041fa97084dbb9fa05e06d4fe3b1d5e2
Charlotte M. Marsh <cmarsh@uni-bremen.de>
2018-08-09 19:34:27 +0200

    Files for the test_mosule are now under version control. Renamed executescript to executer.

---
+++ functions/executer.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Executable script for solving the schrodinger equation
+
+@author: timo
+"""
+
+
+def SGLsolver(directory):
+    """Reads input file and solves the schrodinger equation for the problem.
+       It saves the interpolated potential, the wavefunctions, eigenvalues,
+       expected value and the uncertainty of of the x-coordinate in separate
+       text documents.
+
+    Args:
+        directory: directory where inputfile is saved
+        and outputfiles will be saved
+
+    Returns:
+
+    """
+    import functions.functions
+    import os.path
+    import numpy as np
+
+    mass, minmax, evalmaxmin, iptype, ipoints =\
+        functions.functions._inputreader(directory)
+
+    potwithx = functions.functions._interpolation(minmax, ipoints, iptype)
+    np.savetxt(os.path.join(directory, "potential.dat"), potwithx)
+
+    eigenval, wavefuncs, expvalues =\
+        functions.functions._eigensolver(evalmaxmin, mass, directory)
+    np.savetxt(os.path.join(directory, "energies.dat"), eigenval)
+    np.savetxt(os.path.join(directory, "wavefuncs.dat"), wavefuncs)
+    np.savetxt(os.path.join(directory, "expvalues.dat"), expvalues)
+
+    return(minmax, evalmaxmin, potwithx, eigenval)
+
+
+def visualize(directory, scaling):
+    """Visualizes the results from the SGLsolver. It reads the textdocuments of
+    potential, energies, wavefunctions and expected values and makes plots.
+
+    Args:
+
+    Returns:
+
+    """
+    import functions.visualizer
+
+    functions.visualizer._visualizer(directory, scaling)
+
+    return()
+
+#directory=''
+#SGLsolver(directory)
+#visualizer()
